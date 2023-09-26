d86a6c9bcc7fd9fca137871c6ef8354c992dd858
Nabeel Mirza <77447920+SBSixteen@users.noreply.github.com>
2023-09-26 20:05:28 +0500

    I am express

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,11 @@ const adminRouter = require("./routes/adminRouter").adminRouter;
 
 app.use(express.json());
 
+app.get("/", (req, res) =>{
+
+  res.status(500).send("It appears that you are gay!");
+
+})
 app.use("/admin", adminRouter);
 
 const PORT = 8080;
