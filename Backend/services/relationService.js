c21f530c9fbe6c1368c2fd90b37a4d6e21a0e8d2
Nabeel Mirza <77447920+SBSixteen@users.noreply.github.com>
2023-10-03 16:55:20 +0500

    relationService Completed, Database modifed
    - [DATABASE] : The STUDENT_GUARDIAN table now contains a new field called flag. Change mentioned at the end of SQL file.
    --
    - [PATCH] - deleteAllGuardianRelations
    - [PATCH] - restoreAllGuardianRelations
    - [PATCH] - deleteAllStudentRelations
    - [PATCH] - restoreAllStudentRelations
    - [PATCH] - updateRelationNameByID
    - [PATCH] - updateExistingRelationshipType
    - [GET] - getRelationByStudentID
    - [GET] - getRelationByGuardianID
    
    All APIs tested. Check Postman > DB Project Test > Relation

--- Backend/services/relationService.js
+++ Backend/services/relationService.js
@@ -112,21 +112,20 @@ const updateRelationByName = (req, res) => {
                 }
 
                 return res.status(200).json({
-                    message: "Successfully added a new relationship!"
+                    message: "Successfully updated relationship name!"
                 })
 
             })
 
         }
         else {
-            return res.status(500).json({ message: "DataDuplicationException: Relation DNE!" });
+            return res.status(500).json({ message: "MissingDataException: Relation DNE!" });
         }
 
     })
 
 }
 
-//Test : Will not use most probably
 const getRelationByID = (req, res) =>{
 
     var obj = req.params;
@@ -249,10 +248,576 @@ const assignStudentGuardian = (req, res) => {
 
 }
 
+const getRelationByGuardianID = (req, res) => {
+
+    obj = req.params;
+
+    if(!obj.guardianID) {
+        return res.status(400).json({message: "MissingInputException: GuardianID is required!"});
+    }
+
+    db.query({
+        sql:"SELECT * FROM ?? WHERE ?? = ?",
+        values:[
+            "GUARDIAN",
+            "GUARDIAN_ID",
+            obj.guardianID
+        ]
+    },(errors, results, fields) =>{
+
+        if(errors){
+            return res.status(400).json({message:"Internal Server Error!"});
+        }
+
+        if(results.length == 0){
+            return res.status(200).json({message: "MissingDataException: No such guardian exists!"});
+        }
+
+        db.query({
+            sql:"SELECT * FROM ?? WHERE ?? = ? AND ?? = ?",
+            values:[
+                "STUDENT_GUARDIAN",
+                "GUARDIAN_ID",
+                obj.guardianID,
+                "FLAG",
+                1
+            ]
+        }, (errors, results, fields)=>{
+    
+            if(errors){
+                return res.status(400).json({message:"Internal Server Error!"});
+            }
+    
+            if(results.length == 0){
+                return res.status(200).json({message: "MissingDataWarning: Guardian has no assigned protectorate(s)!"});
+            }
+    
+            return res.status(200).json({results});
+
+        })
+
+    } )
+}
+
+const getRelationByStudentID = (req, res) => {
+
+    obj = req.params;
+
+    if(!obj.studentID) {
+        return res.status(400).json({message: "MissingInputException: GuardianID is required!"});
+    }
+
+    db.query({
+        sql:"SELECT * FROM ?? WHERE ?? = ?",
+        values:[
+            "STUDENT",
+            "STUDENT_ID",
+            obj.studentID
+        ]
+    },(errors, results, fields) =>{
+
+        if(errors){
+            return res.status(400).json({message:"Internal Server Error!"});
+        }
+
+        if(results.length == 0){
+            return res.status(200).json({message: "MissingDataException: No such student exists!"});
+        }
+
+        db.query({
+            sql:"SELECT * FROM ?? WHERE ?? = ? AND ?? = ?",
+            values:[
+                "STUDENT_GUARDIAN",
+                "STUDENT_ID",
+                obj.studentID,
+                "FLAG",
+                1
+            ]
+        }, (errors, results, fields)=>{
+    
+            if(errors){
+                return res.status(400).json({message:"Internal Server Error!"});
+            }
+    
+            if(results.length == 0){
+                return res.status(200).json({message: "MissingDataWarning: Student has no assigned guardians(s)!"});
+            }
+    
+            return res.status(200).json({results});
+
+        })
+
+    } )
+}
+
+const deleteAllStudentRelations = (req, res) =>{
+
+    var obj = req.body;
+
+    if(!obj.studentID){
+        return res.status(404).json({message:"MissingInputException: StudentID is required!"});
+    }
+
+    db.query(
+        {
+            sql: "SELECT * FROM ?? WHERE ?? = ?",
+            values:[
+                "STUDENT",
+                "STUDENT_ID",
+                obj.studentID
+            ]
+        }, (errors, results, fields)=>{
+
+            if(errors){
+                return res.status(500).json({message:"Internal Server Error!"});
+            }
+
+            if(results.length == 0){
+                return res.status(400).json({message: "MissingDataException: No such student in the database!"})
+            }
+
+            db.query({
+                sql:"UPDATE ?? SET ?? = ? WHERE ?? = ?",
+                values:[
+                    "STUDENT_GUARDIAN",
+                    "FLAG",
+                    0,
+                    "STUDENT_ID",
+                    obj.studentID
+                ]
+            }, (errors, results, fields) =>{
+
+                if(errors){
+                    return res.status(500).json({message:"Internal Server Error!"});
+                }
+
+                return res.status(200).json({message: "Deleted all matching student's guardians."})
+
+            }   
+            )
+
+        }
+    )
+
+}
+
+const deleteAllGuardianRelations = (req, res) =>{
+
+    var obj = req.body;
+
+    if(!obj.guardianID){
+        return res.status(404).json({message:"MissingInputException: GuardianID is required!"});
+    }
+
+    db.query(
+        {
+            sql: "SELECT * FROM ?? WHERE ?? = ?",
+            values:[
+                "GUARDIAN",
+                "GUARDIAN_ID",
+                obj.guardianID
+            ]
+        }, (errors, results, fields)=>{
+
+            if(errors){
+                return res.status(500).json({message:"Internal Server Error!"});
+            }
+
+            if(results.length == 0){
+                return res.status(400).json({message: "MissingDataException: No such guardian in the database!"})
+            }
+
+            db.query({
+                sql:"UPDATE ?? SET ?? = ? WHERE ?? = ?",
+                values:[
+                    "STUDENT_GUARDIAN",
+                    "FLAG",
+                    0,
+                    "GUARDIAN_ID",
+                    obj.guardianID
+                ]
+            }, (errors, results, fields) =>{
+
+                if(errors){
+                    return res.status(500).json({message:"Internal Server Error!"});
+                }
+
+                return res.status(200).json({message: "Deleted all matching guardian's protectorate(s)."})
+
+            }   
+            )
+
+        }
+    )
+
+}
+
+const restoreAllStudentRelations = (req, res) =>{
+
+    var obj = req.body;
+
+    if(!obj.studentID){
+        return res.status(404).json({message:"MissingInputException: StudentID is required!"});
+    }
+
+    db.query(
+        {
+            sql: "SELECT * FROM ?? WHERE ?? = ?",
+            values:[
+                "STUDENT",
+                "STUDENT_ID",
+                obj.studentID
+            ]
+        }, (errors, results, fields)=>{
+
+            if(errors){
+                return res.status(500).json({message:"Internal Server Error!"});
+            }
+
+            if(results.length == 0){
+                return res.status(400).json({message: "MissingDataException: No such student in the database!"})
+            }
+
+            db.query({
+                sql:"UPDATE ?? SET ?? = ? WHERE ?? = ?",
+                values:[
+                    "STUDENT_GUARDIAN",
+                    "FLAG",
+                    1,
+                    "STUDENT_ID",
+                    obj.studentID
+                ]
+            }, (errors, results, fields) =>{
+
+                if(errors){
+                    return res.status(500).json({message:"Internal Server Error!"});
+                }
+
+                return res.status(200).json({message: "Restored all matching student's guardians."})
+
+            }   
+            )
+
+        }
+    )
+
+}
+
+const restoreAllGuardianRelations = (req, res) =>{
+
+    var obj = req.body;
+
+    if(!obj.guardianID){
+        return res.status(404).json({message:"MissingInputException: GuardianID is required!"});
+    }
+
+    db.query(
+        {
+            sql: "SELECT * FROM ?? WHERE ?? = ?",
+            values:[
+                "GUARDIAN",
+                "GUARDIAN_ID",
+                obj.guardianID
+            ]
+        }, (errors, results, fields)=>{
+
+            if(errors){
+                return res.status(500).json({message:"Internal Server Error!"});
+            }
+
+            if(results.length == 0){
+                return res.status(400).json({message: "MissingDataException: No such guardian in the database!"})
+            }
+
+            db.query({
+                sql:"UPDATE ?? SET ?? = ? WHERE ?? = ?",
+                values:[
+                    "STUDENT_GUARDIAN",
+                    "FLAG",
+                    1,
+                    "GUARDIAN_ID",
+                    obj.guardianID
+                ]
+            }, (errors, results, fields) =>{
+
+                if(errors){
+                    return res.status(500).json({message:"Internal Server Error!"});
+                }
+
+                return res.status(200).json({message: "Restored all matching guardian's protectorate(s)."})
+
+            }   
+            )
+
+        }
+    )
+
+}
+
+const deleteSpecificRelation = (req,res) =>{
+    
+    var obj = req.body;
+
+    if(!obj.studentID){
+        return res.status(404).json({message:"MissingInputException: StudentID is required!"});
+    }
+    if(!obj.guardianID){
+        return res.status(404).json({message:"MissingInputException: guardianID is required!"});
+    }
+
+    db.query({
+        sql:"SELECT * FROM ?? WHERE ?? = ? AND ?? = ?",
+        values: [
+            "STUDENT_GUARDIAN",
+            "STUDENT_ID",
+            obj.studentID,
+            "GUARDIAN_ID",
+            obj.guardianID
+        ]
+    }, (errors, results, fields) =>{
+
+        if(errors){
+            return res.status(500).json({message:"Internal Server Error!"});
+        }
+
+        if(results.length == 0){
+            return res.status(400).json({message: "MissingDataException: No such relation in the database!"})
+        }
+
+        db.query({
+            sql: "UPDATE ?? SET ?? = ? WHERE ?? = ? AND ?? = ?",
+            values : [
+                "STUDENT_GUARDIAN",
+                "FLAG",
+                0,
+                "STUDENT_ID",
+                obj.studentID,
+                "GUARDIAN_ID",
+                obj.guardianID
+            ]
+        }, (errors, results, fields) =>{
+
+            if(errors){
+                return res.status(500).json({message:"Internal Server Error!"});
+            }
+
+            return res.status(200).json({message: "Successfully deleted relation!"})
+
+        })
+
+    })
+}
+
+const restoreSpecificRelation = (req,res) =>{
+    
+    var obj = req.body;
+
+    if(!obj.studentID){
+        return res.status(404).json({message:"MissingInputException: StudentID is required!"});
+    }
+    if(!obj.guardianID){
+        return res.status(404).json({message:"MissingInputException: guardianID is required!"});
+    }
+
+    db.query({
+        sql:"SELECT * FROM ?? WHERE ?? = ? AND ?? = ?",
+        values: [
+            "STUDENT_GUARDIAN",
+            "STUDENT_ID",
+            obj.studentID,
+            "GUARDIAN_ID",
+            obj.guardianID
+        ]
+    }, (errors, results, fields) =>{
+
+        if(errors){
+            return res.status(500).json({message:"Internal Server Error!"});
+        }
+
+        if(results.length == 0){
+            return res.status(400).json({message: "MissingDataException: No such relation in the database!"})
+        }
+
+        db.query({
+            sql: "UPDATE ?? SET ?? = ? WHERE ?? = ? AND ?? = ?",
+            values : [
+                "STUDENT_GUARDIAN",
+                "FLAG",
+                1,
+                "STUDENT_ID",
+                obj.studentID,
+                "GUARDIAN_ID",
+                obj.guardianID
+            ]
+        }, (errors, results, fields) =>{
+
+            if(errors){
+                return res.status(500).json({message:"Internal Server Error!"});
+            }
+
+            return res.status(200).json({message: "Successfully restored relation!"})
+
+        })
+
+    })
+}
+
+const updateExistingRelationshipType = (req,res) =>{
+
+    var obj = req.body;
+
+    var relationID;
+
+    if(!obj.guardianID){
+        return res.status(404).json({message:"MissingInputException: GuardianID is required!"});
+    }
+    if(!obj.studentID){
+        return res.status(404).json({message:"MissingInputException: StudentID is required!"});
+    }
+    if(!obj.newRelationName){
+        return res.status(404).json({message:"MissingInputException: NewRelationName is required!"});
+    }
+
+    db.query(
+        {
+            sql: "SELECT * FROM ?? WHERE ?? = ?",
+            values:[
+                "RELATION",
+                "RELATION_TYPE",
+                obj.newRelationName
+            ]
+        }, (errors, results, fields) =>{
+        
+            
+            if(errors){
+                return res.status(500).json({message:"Internal Server Error!"});
+            }
+            
+            if(results.length == 0){
+                return res.status(400).json({message:"MissingDataException: No such relation type exists!"});
+            }
+
+            relationID = results[0].RELATION_ID;
+
+            db.query(
+                {
+                    sql:"SELECT * FROM ?? WHERE ?? = ? AND ?? = ?",
+                    values:[
+                        "STUDENT_GUARDIAN",
+                        "STUDENT_ID",
+                        obj.studentID,
+                        "GUARDIAN_ID",
+                        obj.guardianID
+                    ]
+                }, (errors, results, fields) =>{
+                
+                    if(errors){
+                        return res.status(500).json({message:"Internal Server Error!"});
+                    }
+                    
+                    if(results.length == 0){
+                        return res.status(400).json({message:"MissingDataException: No such relation exists!"});
+                    }
+
+                    db.query(
+                        {
+                            sql:"UPDATE ?? SET ?? = ? WHERE ?? = ? AND ?? = ?",
+                            values:[
+                                "STUDENT_GUARDIAN",
+                                "RELATION_ID",
+                                relationID,
+                                "STUDENT_ID",
+                                obj.studentID,
+                                "GUARDIAN_ID",
+                                obj.guardianID
+                            ]
+                        }, (errors, results, fields)=>{
+
+                            if(errors){
+                                return res.status(500).json({message:"Internal Server Error!"});
+                            }
+
+                            return res.status(200).json({message: "Successfully updated relation type of relationship!"})
+
+                        }
+                    )
+
+                }
+            )
+
+            
+        }
+    )
+}
+
+const updateRelationNameByID = (req, res) =>{
+
+    var obj = req.body;
+
+    if(!obj.relationID){
+        return res.status(404).json({message:"MissingInputException: RelationID is required!"});
+    }
+    if(!obj.newRelationName){
+        return res.status(404).json({message:"MissingInputException: NewRelationName is required!"});
+    }
+
+    db.query(
+        {
+            sql:"SELECT * FROM ?? WHERE ?? = ?",
+            values: [
+                "RELATION",
+                "RELATION_ID",
+                obj.relationID,
+            ]
+        }, (errors, results, fields)=>{
+
+            if(errors){
+                return res.status(500).json({message:"Internal Server Error!"});
+            }
+            
+            if(results.length == 0){
+                return res.status(400).json({message:"MissingDataException: No such relation type exists!"});
+            }
+
+            db.query(
+                {
+                    sql:"UPDATE ?? SET ?? = ? WHERE ?? = ?",
+                    values: [
+                        "RELATION",
+                        "RELATION_TYPE",
+                        obj.newRelationName,
+                        "RELATION_ID",
+                        obj.relationID
+                    ]
+                }, (errors, results, fields)=>{
+
+                    if(errors){
+                        return res.status(500).json({message:"Internal Server Error!"});
+                    }
+
+                    return res.status(200).json({message: "Successfully updated relation name!"})
+                }
+            )
+
+        }
+        )
+
+}
+
 module.exports = {
     createRelation,
     getRelations,
     updateRelationByName,
     getRelationByID,
+    getRelationByGuardianID,
+    getRelationByStudentID,
     assignStudentGuardian,
+    deleteAllGuardianRelations,
+    deleteAllStudentRelations,
+    deleteSpecificRelation, 
+    restoreSpecificRelation,
+    restoreAllStudentRelations,
+    restoreAllGuardianRelations,
+    updateExistingRelationshipType,
+    updateRelationNameByID
 };
