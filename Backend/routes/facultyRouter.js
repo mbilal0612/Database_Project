4f4b74af6bb9c4b5aeae0bec15565aa034aa515e
Muhammad Bilal <65731147+stubborn-coder@users.noreply.github.com>
2023-10-27 15:49:08 +0500

    Added plo service
    tested

--- Backend/routes/facultyRouter.js
+++ Backend/routes/facultyRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 questionService = require("../services/questionService");
 questionTypeService = require("../services/questionTypeService");
 cloService = require("../services/cloService");
-
+ploService = require("../services/ploService");
 
 router.get("/getAllQuestions",tryCatch(questionService.getAllQuestions));
 router.get("/getQuestion/:questionId", tryCatch(questionService.getQuestionById));
@@ -14,17 +14,23 @@ router.get("/getAllQuestionTypes", tryCatch(questionTypeService.getAllQuestionTy
 
 router.get("/getAllCLO", tryCatch(cloService.getAllClo));
 router.get("/getClo/:cloId", tryCatch(cloService.getCloById));
+router.get("/getAllPlos", tryCatch(ploService.getPLOs));
+router.get("/getplo/:ploID", tryCatch(ploService.getPLOByID));
 
 router.post("/createQuestion", tryCatch(questionService.createQuestion));
 router.post("/createClo", tryCatch(cloService.createClo));
 router.post("/linkCLOtoQuestion", tryCatch(questionService.assignCLOToQuestion));
 router.post("/createQuestionType", tryCatch(questionTypeService.createQuestionType));
+router.post("/createPlo", tryCatch(ploService.createPLO));
 
 router.put("/updateQuestion", tryCatch(questionService.updateQuestion));
 router.put("/updateClo", tryCatch(cloService.updateClo));
 router.put("/updateQuestionType", tryCatch(questionTypeService.updateQuestionType));
+router.put("/updatePlo",tryCatch(ploService.updatePLO));
+
 
 router.delete("/deleteQuestionType", tryCatch(questionTypeService.deleteQuestionType));
+router.delete("/deletePlo/:ploID", tryCatch(ploService.deletePLO));
 
 
 module.exports = router;
---
+++ Backend/services/ploService.js
@@ -0,0 +1,141 @@
+const db = require('../config/db').connection;
+
+/**
+ * 
+ * @param {body:{ ploName, ploDesc}} req needs req.body.ploName and req.body.ploDesc
+ * @param {*} res 
+ * @returns sucess - status 200 otherwise 400 or 500.
+ */
+const createPLO = (req, res) => {
+  obj = req.body;
+  if (!obj.ploName) {
+    return res.status(400).json({ message: "Need plo to add to table" });
+  }
+
+  if(!obj.ploDesc){
+    return res.status(400).json({message: "PLO Description required!"});
+  }
+  db.query({
+    sql: "INSERT INTO ?? (??,??) VALUES (?,?)",
+    timeout:40000,
+    values: ["PLO", "PLO_NAME", "PLO_DESC", obj.ploName, obj.ploDesc],
+  }, (error, results, fields) => {
+    if (error) return res.status(500).json({ message: "unkown error!" });
+    return res.status(200).json({ message: "PLO successfully added" });
+  });
+};
+
+const getPLOs = (req, res) => {
+
+    db.query({
+        sql: "SELECT * FROM ??",
+        timeout:40000,
+        values: ["PLO"],
+    }, (error, results, fields) => {
+        if (error) return res.status(500).json({ message: "unkown error!" });
+        return res.status(200).json(results);
+    });
+}
+
+
+
+/**
+ * Retrieves a PLO by its ID from the database.
+ * @param {Object} req - The request object.
+ * @param {Object} req.params - The parameters object containing the PLO ID.
+ * @param {string} req.params.ploID - The ID of the PLO to retrieve.
+ * @param {Object} res - The response object.
+ * @returns {Object} The retrieved PLO object.
+ */
+const getPLOByID = (req, res) => {
+
+    const obj = req.params;
+
+    if(!obj.ploID){
+        return res.status(400).json({message: "PLO ID required!"});
+    }
+
+    db.query({
+        sql: "SELECT * FROM ?? WHERE ?? = ?",
+        timeout:40000,
+        values: ["PLO", "PLO_ID", obj.ploID]
+    },
+    (error, results, fields) => {
+        if(error){
+            console.log(error);
+            return res.status(500).json({message: "server error!"});
+        }
+
+        return res.status(200).json(results);
+    })
+};
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
+const updatePLO = (req, res) => {
+    
+  const obj = req.body;
+
+  if(!obj.ploID){
+      return res.status(400).json({message: "PLO ID required!"});
+  }
+  if(!obj.ploName){
+    return res.status(400).json({ message: "PLO Name required!" });
+  }
+
+  if(!obj.ploDesc){
+    return res.status(400).json({ message: "PLO Description required!" });
+  }
+
+  db.query({
+    sql: "UPDATE ?? SET ?? = ?, ?? = ? WHERE ?? = ?",
+    timeout:40000,
+    values: ["PLO", "PLO_NAME", obj.ploName, "PLO_DESC", obj.ploDesc, "PLO_ID", obj.ploID]
+  },
+  (error,results, fields) => {
+    
+    if(error){
+      return res.status(500).json({message: "server error!"});
+    }
+    if(results.affectedRows == 0){
+      return res.status(400).json({message: "PLO not found!"});
+    }
+    return res.status(200).json({message: "PLO updated successfully!"});
+
+  })
+
+}
+
+const deletePLO = (req, res) => {
+  
+    const obj = req.params;
+  
+    if(!obj.ploID){
+      return res.status(400).json({message: "PLO ID required!"});
+    }
+  
+    db.query({
+      sql: "DELETE FROM ?? WHERE ?? = ?",
+      timeout:40000,
+      values: ["PLO", "PLO_ID", obj.ploID]
+    },
+    (error,results, fields) => {
+      
+      if(error){
+        return res.status(500).json({message: "server error!"});
+      }
+      return res.status(200).json({message: "PLO deleted successfully!"});
+  
+    })
+}
+
+module.exports ={
+    createPLO,
+    getPLOs,
+    getPLOByID,
+    updatePLO,
+    deletePLO
+}
